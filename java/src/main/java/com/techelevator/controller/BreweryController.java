5ce767486d9f73bdab9fe6fc0c417c49d6af0a54
Nathan Potchinsky <NathanPotchinsky@gmail.com>
2022-09-05 05:46:10 -0400

    added BreweryController and CrossOrigin annotation

--- java/src/main/java/com/techelevator/controller/BreweryController.java
+++ java/src/main/java/com/techelevator/controller/BreweryController.java
@@ -2,11 +2,12 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.BreweryDao;
 import com.techelevator.model.Brewery;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin
 @RestController
 public class BreweryController {
     private BreweryDao breweryDao;
@@ -20,4 +21,33 @@ public class BreweryController {
         List<Brewery> breweries = breweryDao.findAll();
         return breweries;
     }
+
+    @GetMapping(value = "/breweries/{id}")
+    public Brewery getBreweryById(@PathVariable long id) {
+        Brewery brewery = breweryDao.getBreweryById(id);
+        return brewery;
+    }
+
+    @GetMapping(value = "breweries/{name}")
+    public Brewery getBreweryByName(String name) throws Exception {
+        Brewery brewery = breweryDao.getBreweryByName(name);
+        return brewery;
+    }
+
+    @PostMapping(value = "/breweries")
+    public Boolean createBrewery(@RequestBody Brewery brewery) {
+        return breweryDao.create(brewery.getBreweryName(), brewery.getBreweryStreet(), brewery.getBreweryCity(), brewery.getBreweryState(),
+                brewery.getBreweryPostCode(), brewery.getBreweryHours());
+    }
+
+    @PutMapping(value = "/breweries/{id}/hours")
+    public void updateHours(@PathVariable long id, @RequestBody Map<Integer, String[]> newHours){
+        if (breweryDao.getBreweryById(id).getBreweryHours().isEmpty()){
+            breweryDao.createHours(id, newHours);
+        }
+        else {
+            breweryDao.updateHours(id,newHours);
+        }
+    }
+
 }
