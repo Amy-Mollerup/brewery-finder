800e011d4ee76546756290a73554e1e5d4869c71
Nathan Potchinsky <NathanPotchinsky@gmail.com>
2022-09-14 00:28:31 -0400

    added database files

--- java/database/breweries_data.sql
+++
@@ -1,7 +0,0 @@
-copy breweries
-from "paste path to breweries.csv here"
-delimiter ','
-CSV;
-
-SELECT SETVAL
-((SELECT PG_GET_SERIAL_SEQUENCE('breweries', 'id')), (SELECT (MAX(id) + 1) FROM breweries), FALSE);
