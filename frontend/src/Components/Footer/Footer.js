d2c20c2c62e822579222269035a43adf0a2ec9c7
Ishwor Tuladhar <ishwor.tuladhar@gmail.com>
2022-09-14 20:13:47 -0400

    Footer page preview is ready

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,10 +1,107 @@
-import React from 'react';
+import React from "react";
+import "./FooterStyle.css";
+import { Row, Col } from "reactstrap";
 
 export default function Footer() {
-    return (
-        <footer>
-            <h2 className='testimonial'>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.”</h2>
-            <p>Designed and Maintained by _______</p>
-        </footer>
-    )
+  return (
+    <div className="footer-card">
+ 
+     
+      <Row>
+      
+        <Col>
+          <div className="footer--head1">Featured on </div>
+          <p className="break-line" />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col
+          className=""
+          md={{
+            offset: 3,
+            size: 9,
+          }}
+          sm="12"
+        >
+          <div className="footer-card-content">
+            <img
+              src={require("../assets/NicePng_white-png_135300.png")}
+              height={30}
+              width={85}
+              className="business--logo"
+              alt=""
+            />
+            <img
+              src={require("../assets/NicePng_direct-tv-logo-png_2230304.png")}
+              height={35}
+              width={70}
+              className="business--logo"
+              alt=""
+            />
+            <img
+              src={require("../assets/NicePng_tripadvisor-logo-png_9588046.png")}
+              height={42}
+              width={90}
+              className="business--logo"
+              alt=""
+            />
+            <img
+              src={require("../assets/NicePng_mountain-drawing-png_2943116.png")}
+              height={65}
+              width={85}
+              className="business--logo"
+              alt=""
+            />
+        
+          </div>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col
+          className=""
+          md={{
+            offset: 0
+          }}
+          sm="12"
+        >
+          <p>
+            <small className="follow-text">
+            To find a local brewery, start by looking it up online. Using Brewery Finder is an easy way 
+            to learn about different types of beers and locate nearby breweries. It also works as a tool for 
+            finding new beers or rating your favorites on a scale of 1 to 10+. Be sure to check out these 
+            other resources if you want to learn more! lists over 13,000 beers from around the world, graded 
+            on a scale of 100 points each. It shows each brew's style, country of origin, age requirement, 
+            alcohol percentage, price and more information about what makes this distinct beverage unique to 
+            its culture?
+            </small>
+          </p>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col
+          className=""
+          md={{
+            offset: 1,
+            size: 7,
+          }}
+          sm="12"
+        >
+          <div>
+            <p>
+              <small className="follow-text">FOLLOW US ON SOCIAL MEDIA</small>{" "}
+              <label className="footer-right">
+                <i className="fab fa-instagram" />
+                <i className="fab fa-facebook-square" />
+                <i className="fab fa-linkedin" />
+                <i className="fab fa-twitter-square" />
+              </label>
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
