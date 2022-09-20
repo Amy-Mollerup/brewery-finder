02e2b51f218722bad78438e0049ad00a683967c1
Ishwor Tuladhar <ishwor.tuladhar@gmail.com>
2022-09-20 00:52:59 -0400

    Reviwer's review list with stars and style is ready to review

--- frontend/src/Pages/BreweryBeerListPage/BrewerAccessReviewComponent/BrewerAccessReviewComp.js
+++ frontend/src/Pages/BreweryBeerListPage/BrewerAccessReviewComponent/BrewerAccessReviewComp.js
@@ -1,61 +1,64 @@
 import React, { useEffect } from "react";
-import { Row, Col} from "reactstrap";
+import { Row, Col } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./BrewerAccessReviewStyle.css";
 import axios from "axios";
 
-
 const BrewerAccessReviewComp = (props) => {
-
   const [reviews, setReviews] = React.useState([]);
-  
-  function getReviews(){
-    const url = 'http://localhost:8081/beer/' + props.beerId + '/reviews'
-    axios.get(url, [])
-    .then(resp => {
-        setReviews(resp.data)
-    })
+
+  function getReviews() {
+    const url = "http://localhost:8081/beer/" + props.beerId + "/reviews";
+    axios.get(url, []).then((resp) => {
+      setReviews(resp.data);
+    });
   }
 
-  useEffect(() => {getReviews()}, [])
+  useEffect(() => {
+    getReviews();
+  }, []);
 
   const reviewLists = reviews.map((item) => (
-    <div className="row" key={item.beerId}>
-      <div className="col-lg-5">
+    <Row  key={item.beerId}>
+      <Col className="col-md-11">
         <div
-          className="box d-flex align-items-center wow fadeInLeft"
-          id="rev--box"
-        >
-          <div className="testimonials">
-            {/* <h3>
-              {item.username}
-              <span>Beer Lover</span>
-            </h3> */}
+          className= "BrewerAccessReview--box">
+       
+            {new Array(item.rating).fill(null).map(() => (
+              <i
+                className="fas fa-star"
+                style={{ color: "orange", fontSize: "20px" }}
+              />
+            ))} 
+            <br />
             <p>{item.review}</p>
+
+
+            <h6>- {item.id}</h6>
           </div>
-          {/* <div className="image">
-            <img
-              src="https://i.ibb.co/8x9xK4H/team.jpg"
-              alt=""
-              className="img-fluid"
-              id="rev--image"
-            />
-          </div> */}
-        </div>
-      </div>
-    </div>
+        
+      </Col>
+    </Row>
   ));
 
-  return (
-    <div className="testimonials" id="reviewer--container">
-      <Col className="heading wow fadeIn text-center" fluid="sm">
-        <h2>
-          Beer Lovers'
-          <span> Reviews</span>
-        </h2>
-      </Col>
 
-      <Row className="reviewlist" md="1"> {reviewLists} </Row>
+  return (
+    <div className="BrewerAccessReview--container">
+      <Row>
+        <Col
+          className="heading wow fadeIn text-center"
+          id="reviewer--container"
+        >
+          <h2>
+            Beer Lovers'
+            <span> Reviews</span>
+          </h2>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col className=""> {reviewLists}</Col>
+      </Row>
     </div>
   );
 };
